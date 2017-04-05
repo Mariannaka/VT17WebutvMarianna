5bb208de2c526ba7ba92ac67f7b7e76534b54f10
Marianna Khachadourian <mariannakhachadourian@gmail.com>
2017-04-05 12:08:13 +0200

    vote message

--- VT17WebutvMarianna/assets/script/examination.js
+++ VT17WebutvMarianna/assets/script/examination.js
@@ -9,11 +9,44 @@
 //select_button.addEventListener("click", function());
 
 
-function changePortion() {
-	var selectedPortion = portions_list.value;
-	
+window.onload = displayLoader();
+
+function displayLoader(){
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function (){
+
+        if(this.readyState === 4 && this.status === 200){
+            var json = JSON.parse(this.responseText);
+
+            var ratingtofixed = json.rating.toFixed(3);
+            document.getElementById("votesmessage").innerText = "Recipe: " + json.recipe + ". Votes: " + json.votes + " Rating:  " + ratingtofixed;
+        }
+    }
+
+    xhttp.open("GET",
+                                                "https://edu.oscarb.se/sjk15/api/recipe/?api_key=0c964dd5d9fb9898&recipe=lussekatter",
+                                                true);
+
+    xhttp.send();
 }
 
-function functionrait(int){
-	console.log(int);
+function functionrait(int) {
+
+    xmlhttp=new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange=function() {
+        if (this.readyState==4 && this.status==200) {
+            displayLoader();
+            display(int);
+            localStorage.data = "votes is: "+int;
+        }
+    }
+    xmlhttp.open("GET","https://edu.oscarb.se/sjk15/api/recipe/?api_key=0c964dd5d9fb9898&recipe=lussekatter&rating="+int,true);
+    xmlhttp.send();
+
+}
+function display(value){
+    document.getElementById("uservotes").innerText = "Your votes is: " + value;
 }
