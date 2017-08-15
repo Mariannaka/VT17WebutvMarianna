cde0f4fe1d9c9797c2dda55a8f9ad77f622f5c67
Marianna Khachadourian <mariannakhachadourian@gmail.com>
2017-08-15 13:35:32 +0200

    uppgift7 (examination)
    fixed and rewrote some missing codes..

--- VT17WebutvMarianna/assets/script/uppgift5.js
+++ VT17WebutvMarianna/assets/script/uppgift5.js
@@ -1,66 +1,49 @@
-var pnumber = document.getElementById("pnumber");
-var year = document.getElementById("year");
 
-var click-pnumber = document.getElementById ("click-pnumber");
-var click-year = document.getElementById ("click-year");
-var click-numbers = document.getElementById ("click-numbers");
 
-click-numbers.addEventListener("input", checkPnumber);
-click-year.addEventListener("input", isLeapYear); //try input action
-click-numbers.addEventListener("input", calcNumbers);
 
-function.isLeapYear (e){
-	e.preventDefault();
-	var leapYear= parseInt(document.getElementById("year").value);
+function checkPnumber(){
+	console.log("first function");
+	var myNumber = document.getElementById("pnumber").value;
+	console.log(myNumber);
+}
 
-	if (document.getElementById("year".value==" ")) {
+
+
+function checkLeapYear(){
+	var myYear = document.getElementById("year-value").value;
+
+	if (myYear==="") {
 		alert ("Enter a year!");
 		return false;
 	}
 
-	if (leapYear >=1000 && leapYear <=3000) {
-
-		if ((leapYear % 4 == 0) && (leapYear % 100 != 0)) || (leapYear % 400 == 0) {
-			alert("This is a Leap Year!");
-		} else {
-			alert("This is NOT a Leap Year!");
-			return true;
+	if (((myYear % 4 == 0) && (myYear % 100 != 0)) || (myYear % 400 == 0)) {
+		document.getElementById("year-label").innerHTML = "This is a Leap Year!";
+		return true;
+	} 
+		else {
+			document.getElementById("year-label").innerHTML = "This is NOT a Leap Year!";
+			return false;
 		} 
 
-	} else {
-		alert ("Unvalid year! choose a year between 1000-3000.");
-		return false;
 	}
-}
-
-function.checkPnumber (e){
-		e.preventDefault(); 
 
 
-}
+
+	function resetLabel() {
+		document.getElementById("year-label").innerHTML = "";
+	}
 
 
-function.calcNumbers (e){
-		e.preventDefault();
-		var numbers = (document.getElementById("numbers").value);
-		var total=0;
 
-	for (int i=0; i<numbers.length; i++) {
-		var numLoc= numbers.charAt(i);
-		 total+= numLoc;
-	} alert ('The sum of your numbers is: ' + total);
-
-}
-
-
-function calcSum(e){
-		e.preventDefault();
+  function sumUp(){
+		var myNumbers = document.getElementById("number-value").value.toString();
 		var sum = 0;
-		var input = document.getElementById("numberSum").value; //sum
-		while(input){
-			sum += input%10;
-			input = Math.floor(input/10);
+		
+		for (var i = 0; i < myNumbers.length; i++) {
+			sum += parseInt(myNumbers.charAt(i));
 		}
-
-		return document.getElementById("numberSumResult").innerHTML = sum;
+		
+		document.getElementById("sum-up-label").innerHTML = sum ;
+		return sum;
 	}
